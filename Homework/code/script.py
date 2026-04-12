67ace69ef5c2875737482b7227308063f9d8468d
mpiccoli18 <mpiccoli18@gmail.com>
2026-04-12 22:32:35 +0200

    Sort of completed the script for the first exercise. Starting now to write the code for the second exercise

--- Homework/code/script.py
+++ Homework/code/script.py
@@ -3,23 +3,24 @@ import queue
 import matplotlib.pyplot as plt
 import numpy as np
 
-lam = 2 #Lambda for exponential distribution
+lam = 19 #Lambda for exponential distribution
+mu = 20  #Mu for exponential distribution
 
 class Server:
     def __init__(self):
-        self.isBusy = False
+        self.busy = False
         self.currentEvent = None
     
-    def isServerBusy(self):
-        return self.isBusy
+    def isBusy(self):
+        return self.busy
     
     def assignEvent(self, event):
         self.currentEvent = event
-        self.isBusy = True
+        self.busy = True
     
     def releaseEvent(self):
         self.currentEvent = None
-        self.isBusy = False
+        self.busy = False
     
     def getCurrentEvent(self):
         return self.currentEvent
@@ -91,30 +92,92 @@ class randomGenerator:
     def generateUniformRandNum(self):
         return self.rng.random()
 
-    def generateExponentialRandNum(self):
-        return self.rng.exponential(1/lam)
+    def generateExponentialRandNum(self, num):
+        return self.rng.exponential(1/num)
+
+def arrival(currentEvent, currentTime, eventQueue, server, rng, waitingQueue):
+        nextArrivalTime = currentTime + rng.generateExponentialRandNum(lam)
+        nextArrivalTimeEvent = Event("ARRIVAL", nextArrivalTime)
+        eventQueue.addEventbasedOnTimestamp(nextArrivalTimeEvent)
+
+        if server.isBusy():
+            waitingQueue.append(currentTime)
+        else:
+            server.assignEvent(currentEvent)
+            departureTime = currentTime + rng.generateExponentialRandNum(mu)
+            departureEvent = Event("DEPARTURE", departureTime)
+            eventQueue.addEventbasedOnTimestamp(departureEvent)
+
+def departure(currentTime, eventQueue, server, rng, waitingQueue, totalTimeSpent, mu):
+    arrival = server.getCurrentEvent()
+    systemTime = currentTime - arrival.timestamp
+    totalTimeSpent.append(systemTime)
+
+    if len(waitingQueue) == 0:
+        server.releaseEvent()
+    else:
+        nextArrivalTime = waitingQueue.pop(0)
+        server.assignEvent(Event("ARRIVAL", nextArrivalTime))
+
+        serviceTime = rng.generateExponentialRandNum(mu)
+        departureTime = currentTime + serviceTime
+        departureEvent = Event("DEPARTURE", departureTime)
+        eventQueue.addEventbasedOnTimestamp(departureEvent)
+
+def plot(totalTimeSpent):
+    
+    averageRun = []
+    sum = 0
+    for i, time in enumerate(totalTimeSpent):
+        sum += time
+        averageRun.append(sum / (i + 1))
+
+    average = 1 / (mu - lam)
+    plt.plot(averageRun, label="Running Average")
+    plt.axhline(y=average, color='r', linestyle='-', label="Theoretical Average")
+    plt.xlabel("Number of Packets Processed")
+    plt.ylabel("Average Time in System")
+    plt.legend()
+    plt.show()
 
 def main():
     print("Simulation started at: ", datetime.datetime.now())
     print("Intializing event queue, server and random number generator...")
     eventQueue = EventQueue()
     rng = randomGenerator()
+    server = Server()
+    currentTime = 0.0
+    waitingQueue = []
+    totalTimeSpent = []
     
     uniform = rng.generateUniformRandNum()
     print("Uniform number generated: ", uniform)
 
-    exponential = rng.generateExponentialRandNum()
+    exponential = rng.generateExponentialRandNum(lam)
     print("Exponential number generated: ", exponential)
-    
+
+    firstEvent = Event("ARRIVAL", exponential)
+    eventQueue.addEventbasedOnTimestamp(firstEvent)
+    print("First event added to the queue with timestamp: ", exponential)
+
+    MAX = 1000000
+
+    while not eventQueue.isEmpty():
+        currentEvent = eventQueue.removeEvent()
+        currentTime = currentEvent.timestamp
+        
+        if currentTime > MAX:
+            break
+
+        if currentEvent.eventType == "ARRIVAL":
+            arrival(currentEvent, currentTime, eventQueue, server, rng, waitingQueue)
+        
+        elif currentEvent.eventType == "DEPARTURE":
+            departure(currentTime, eventQueue, server, rng, waitingQueue, totalTimeSpent, mu)
 
     
-
-    
-
-
-
-
-
+    print("Simulation ended at: ", datetime.datetime.now())
+    plot(totalTimeSpent)
 
 
 if __name__ == "__main__":

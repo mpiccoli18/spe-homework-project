a18c2a81aab401b3874a1afe7910f148db7ebc71
mpiccoli18 <mpiccoli18@gmail.com>
2026-04-16 23:40:26 +0200

    small changes

--- Homework/code/script.py
+++ Homework/code/script.py
@@ -4,9 +4,9 @@ import matplotlib.ticker as ticker
 import numpy as np
 import math
 
-lam = 1      #Lambda for exponential distribution
-lam2 = 0.2      #Lambda for exercise 2: lam2 << M
-mu = 2          #Mu for exponential distribution
+lam = 8         #Lambda for exponential distribution
+lam2 = 0.22         #Lambda for exercise 2: lam2 << M
+mu = 10              #Mu for exponential distribution
 
 class Server:
     def __init__(self):
@@ -74,18 +74,9 @@ class EventQueue:
             self.rear = None
         self.size -= 1
         return removedEvent
-    
-    def getFront(self):
-        return self.front
-
-    def getRear(self):
-        return self.rear
 
     def isEmpty(self):
         return self.front is None
-    
-    def getSize(self):
-        return self.size
     
 class randomGenerator:
     def __init__(self):
@@ -184,7 +175,7 @@ def confidenceInterval(totalTimeSpent, batch=500):
     return lowerBound, upperBound
 
 def plot(totalTimeSpent, confidence, isFirstEx = True):
-    rate = 750
+    rate = 1000
     averageRun = []
     sum = 0
     for i, time in enumerate(totalTimeSpent):
@@ -199,36 +190,33 @@ def plot(totalTimeSpent, confidence, isFirstEx = True):
     CI = confidence
     plt.fill_between(axisX, CI[0], CI[1], color='orange', alpha=0.3, label="Confidence Interval")
 
-    zoomAxis = zoom.inset_axes([0.7, 0.25, 0.25, 0.25])
+    zoomAxis = zoom.inset_axes([0.5, 0.2, 0.3, 0.2])
     zoomAxis.plot(axisX, averageRun, color='b', linewidth=1.5)
     zoomAxis.fill_between(axisX, CI[0], CI[1], color='orange', alpha=0.3)
     minZoom = len(totalTimeSpent) * 0.80
     maxZoom = len(totalTimeSpent)
     zoomAxis.set_xlim(minZoom, maxZoom)
-    minY = CI[0] - 0.015
-    maxY = CI[1] + 0.015
+    minY = CI[0] - (CI[0] * 5 / 100)
+    maxY = CI[1] + (CI[1] * 5 / 100)
     zoomAxis.set_ylim(minY, maxY)
-    #zoomAxis.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
-    #zoomAxis.yaxis.set_major_locator(ticker.MaxNLocator(nbins=3))
-    #zoomAxis.xaxis.set_major_locator(ticker.MaxNLocator(nbins=3))
     zoomAxis.set_xticks([])
     zoomAxis.set_yticks([])
     zoom.indicate_inset_zoom(zoomAxis, edgecolor="black")
-    
+
     if isFirstEx:
         average = 1 / (mu - lam)
         plt.axhline(y=average, color='r', linestyle='--', label="Theoretical Average", linewidth=2)
         zoomAxis.axhline(y=average, color='r', linestyle='--', linewidth=2)
 
     plt.xlim(None, len(totalTimeSpent))
-    plt.xlabel("Number of Packets Processed", fontsize=12)
-    plt.ylabel("Average Time in System", fontsize=12)
+    plt.xlabel("Number of Packets Processed", fontsize=12, labelpad=15)
+    plt.ylabel("Average Time in System (s)", fontsize=12, labelpad=15)
     plt.gca().xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
     plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
     plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
     plt.gca().tick_params(axis='both', which='major', labelsize=12)
     plt.grid(True, linestyle=":", alpha=0.6)
-    plt.legend(loc="lower right", fontsize=10)
+    plt.legend(loc="best", fontsize=10)
 
     if isFirstEx:
         plt.title("Exercise 1", fontsize=14)
@@ -239,6 +227,7 @@ def plot(totalTimeSpent, confidence, isFirstEx = True):
         plt.savefig("Exercise2.pdf", format="pdf", bbox_inches="tight", transparent=False)
         print("Saved plot as Exercise2.pdf")
     plt.show()
+
 
 def newServiceTime(x):
     if x < 0 or x > 6:
@@ -318,14 +307,13 @@ def main():
     print("Found maximum height M: ", M)
 
     getAvgServiceTime(M, rng)
-    exponential = round(rng.generateExponentialRandNum(lam2), 4)
     
-    print("Exponential number generated: ", exponential)
+    print(f"Exponential number generated: {exponential:.3f}")
 
     firstEvent = Event("ARRIVAL", exponential)
     eventQueue.addEventbasedOnTimestamp(firstEvent)
     
-    print("First event added to the queue with timestamp: ", exponential)
+    print(f"First event added to the queue with timestamp: {exponential:.3f}")
 
     while not eventQueue.isEmpty():
         currentEvent = eventQueue.removeEvent()
